6300dae2320df9cb96121b7fb5f6f7d97960fde3
JohnnieLZ <553348468@qq.com>
2019-01-26 07:40:09 +0800

    工厂方法模式DEMO实现

--- src/com/shrui/factorypattern/factorymethodpattern/Test.java
+++ src/com/shrui/factorypattern/factorymethodpattern/Test.java
@@ -3,6 +3,7 @@ package com.shrui.factorypattern.factorymethodpattern;
 public class Test  {
 
     public static void main(String[] args) {
+        //通过创建不同的子类来进行对象
         FStores fStores = new BenzFStores();
         fStores.drive();
         fStores.carInfo();
